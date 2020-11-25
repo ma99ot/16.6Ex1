d893a92bca573acbb478727d7dc7c92d90d5e213
Mikhail Kolesnikov <mikhailkolesnikov@gmail.com>
2020-11-25 11:08:03 +0300

    Added functions removeNumber and printVector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+
+void printVector (std::vector<int> printed){
+    for (int i = 0; i < printed.size(); i++) {
+        std::cout << printed[i] << " ";
+    }
+}
+
+std::vector<int> removeNumber(std::vector<int> numbers, int remove) {
+    std::vector<int> result;
+    for (int i = 0; i < numbers.size(); i++){
+        if (numbers[i] != remove) result.push_back(numbers[i]);
+    }
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numbersToAdd;
+    std::cout << "Please, input count of numbers" <<  std::endl;
+    std::cin >> numbersToAdd;
+    std::cout << "Please, input "<< numbersToAdd << " numbers" <<  std::endl;
+    std::vector<int> numbers;
+    for (int i = 0; i < numbersToAdd; i++){
+        int insertedNumber;
+        std::cin >> insertedNumber;
+        numbers.push_back(insertedNumber);
+    }
+    int numberToRemove;
+    std::cout << "Please, input number you want to remove:" <<  std::endl;
+    std::cin >> numberToRemove;
+    printVector(removeNumber(numbers, numberToRemove));
     return 0;
 }
